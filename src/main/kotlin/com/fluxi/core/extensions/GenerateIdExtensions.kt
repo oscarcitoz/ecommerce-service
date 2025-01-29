94a0e44c64695380cc71b89ff57d75b616e012c5
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-29 16:09:25 -0500

    crud product

---
+++ src/main/kotlin/com/fluxi/core/extensions/GenerateIdExtensions.kt
@@ -0,0 +1,10 @@
+package com.fluxi.core.extensions
+
+import java.util.UUID
+
+fun generateId(): String {
+    val uuidShort = UUID.randomUUID().toString().replace("-", "").substring(0, 20)
+    val timestampHex = System.currentTimeMillis().toString(16)
+
+    return "$uuidShort-$timestampHex"
+}
