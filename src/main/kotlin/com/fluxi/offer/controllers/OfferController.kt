6f77cb823178017ccf2ea4c8c967f04c788082b1
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-29 17:47:35 -0500

    add crud offers

--- src/main/kotlin/com/fluxi/offer/controllers/OfferController.kt
+++ src/main/kotlin/com/fluxi/offer/controllers/OfferController.kt
@@ -1,12 +1,39 @@
 package com.fluxi.offer.controllers
 
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.*
+import io.micronaut.http.HttpStatus
 import com.fluxi.offer.services.OfferServiceInterface
+import com.fluxi.offer.models.Offer
 
 @Controller("/offer")
 class OfferController(private val offerService: OfferServiceInterface) {
-    
-    @Get("/")
-    fun findAll() = offerService.findAll()
+    @Get("/{id}")
+    fun findById(id: Long): Offer? {
+        return offerService.findById(id)
+    }
+
+    @Post("/")
+    @Status(HttpStatus.CREATED)
+    fun create(@Body offer: Offer, @Header("owner-id") ownerId: String): Offer {
+        offer.ownerId = ownerId
+
+        return offerService.create(offer)
+    }
+
+    @Put("/{id}")
+    fun update(id: Long, @Body offer: Offer, @Header("owner-id") ownerId: String): Offer? {
+        offer.ownerId = ownerId
+
+        return offerService.update(id, offer)
+    }
+
+    @Get("/owner/{ownerId}")
+    fun findByOwnerId(ownerId: String): List<Offer> {
+        return offerService.findByOwnerId(ownerId)
+    }
+
+    @Get("/product/{productId}")
+    fun findByProductId(productId: String): List<Offer> {
+        return offerService.findByProductId(productId)
+    }
 } 
