de3b22db956d58cd02b1a7e0de105a3925fdebf7
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-02-03 23:02:15 -0500

    change required fields.

--- src/main/kotlin/com/fluxi/websites/phases/creations/SaveProductsPhase.kt
+++ src/main/kotlin/com/fluxi/websites/phases/creations/SaveProductsPhase.kt
@@ -21,7 +21,7 @@ class SaveProductsPhase(
         val productSaved = this.createProduct(
             dto,
             dto.request.productName,
-            dto.request.productDescription,
+            dto.request.productDescription ?: dto.request.productName,
             dto.imagesProduct,
             dto.request.productPrice
         )
@@ -31,7 +31,7 @@ class SaveProductsPhase(
                 dto,
                 it.name,
                 it.name,
-                listOf(dto.upsellImage?: ""),
+                listOf(dto.upsellImage ?: ""),
                 it.price
             )
 
